227ec4b1e41eb83ba2e87b32a35bf935d38efb05
Georgi Ivanov <gogbg@outlook.com>
2023-05-24 23:36:01 +0300

    force publish module

--- Publish-Module.ps1
+++ Publish-Module.ps1
@@ -12,4 +12,4 @@ param
 
 #Find Module manifest
 $moduleFolder = Get-Item -Path $ModulePath
-Publish-Module -Path $moduleFolder.FullName -NuGetApiKey ($ApiKey | ConvertFrom-SecureString -AsPlainText) -Repository $Repository -ErrorAction Stop
+Publish-Module -Path $moduleFolder.FullName -NuGetApiKey ($ApiKey | ConvertFrom-SecureString -AsPlainText) -Force -Repository $Repository -ErrorAction Stop
